656587a1e43cdd22712f191dfebe110e64649f51
gwpaeng <gwpaeng@naver.com>
2021-11-17 21:03:07 +0900

    Fix ft_export.c

--- src/ft_export.c
+++ src/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: gpaeng <gpaeng@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 14:53:12 by gpaeng            #+#    #+#             */
-/*   Updated: 2021/11/17 19:55:02 by gpaeng           ###   ########.fr       */
+/*   Updated: 2021/11/17 21:02:51 by gpaeng           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,11 +36,6 @@ void ft_print_env_export(void)
     }
 }
 
-int ft_check_key_value_form(int i, int j, char *args[])
-{
-    return (j == 0 || j == 1 || j == (int)ft_strlen(args[i]) + 1);
-}
-
 int ft_str_digit_check(char *str)
 {
     int i;
@@ -55,14 +50,19 @@ int ft_str_digit_check(char *str)
     return (1); //전체 문자열
 }
 
-char *find_path_name(int i, int j, char *args[])
+int ft_check_key_value_form(int i, int j, char *args[])
 {
-    char *path_name;
-
-    path_name = ft_set_malloc(j + 1);
-    ft_strlcpy(path_name, args[i], j + 1);
-    return (path_name);
+    if (j == 0 || j > (int)ft_strlen(args[i]) - 1)
+    {
+        if (!ft_str_digit_check(args[i]))
+        {
+            printf("minishell: export: \'%s\': not a valid identifier\n", args[i]);
+        }
+        return (1);
+    }
+    return (0); 
 }
+
 
 int ft_check_key_form(int i, int j, char *args[])
 {
@@ -70,14 +70,16 @@ int ft_check_key_form(int i, int j, char *args[])
 
     path_name = ft_set_malloc(j + 1);
     ft_strlcpy(path_name, args[i], j + 1);
-    if (ft_str_digit_check(path_name))
-        return (0);
-    return (1);
+    if (!ft_str_digit_check(path_name))
+    {
+        printf("minishell: export: \'%s\': not a valid identifier\n", args[i]);
+        return (1);
+    }
+    return (0);
 }
 
 int ft_check_arg_form(char *args[])
 {
-    char *path_name;
     int i;
     int j;
 
@@ -85,16 +87,12 @@ int ft_check_arg_form(char *args[])
     while (args[i])
     {
         j = 0;
-        if (args[i][j] != '=')
+        while (args[i][j] != '=')
             j++;
-        if (ft_check_key_value_form(i, j, args)) //없는 것
+        if (ft_check_key_value_form(i, j, args)) // key=value 형태가 아닌것
             return (0);
-        if (ft_check_key_form(i, j, args)) //숫자가 있는 경우
-        {
-            path_name = find_path_name(i, j, args);
-            printf("export: not an identifier: %s\n", path_name);
+        if (ft_check_key_form(i, j, args)) //숫자가 있는 경우   
             return (0);
-        }
         i++;
     }
     return (1);
@@ -107,7 +105,7 @@ void ft_update_env_export(char *args[])
     int cnt_env_arr;
     int i;
 
-    cnt_env_arr = ft_cnt_arg(jop.envp);
+    cnt_env_arr = ft_cnt_arg(jop.envp) + 1;
     env = (char **)malloc(sizeof(char *) * (cnt_env_arr + 1));
     if (!env)
         return ;
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gpaeng <gpaeng@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 19:25:36 by hyeonsok          #+#    #+#             */
-/*   Updated: 2021/11/17 18:02:13 by gpaeng           ###   ########.fr       */
+/*   Updated: 2021/11/17 21:00:36 by gpaeng           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,19 @@ int main(int argc, char *argv[], char *env[])
 	
 	// ft_echo(argv);
 	// ft_pwd(argv);
-	ft_exit(argv);
+	// ft_exit(argv);
 	// int i =0;
 	// while (jop.envp[i])
 	// {
 	// 	printf("?>> %s\n", jop.envp[i]);
 	// 	i++;
 	// }
-	// ft_export(argv);
-	// printf("================\n");
-	// i =0;
-	// while (jop.envp[i])
-	// {
-	// 	printf("?>> %s\n", jop.envp[i]);
-	// 	i++;
-	// }
+	ft_export(argv);
+	int i =0;
+	while (jop.envp[i])
+	{
+		printf("?>> %s\n", jop.envp[i]);
+		i++;
+	}
 	// ft_env(argv);
 }
