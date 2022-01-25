27b3f259e5feea06e78dce10c5675f5296755419
hyeonsok <hyeonsok@student.42seoul.kr>
2022-01-25 17:23:31 +0900

    Implement collecting child processes exit status

--- src/main2.c
+++ src/main2.c
@@ -6,7 +6,7 @@
 /*   By: hyeonsok <hyeonsok@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 22:51:09 by hyeonsok          #+#    #+#             */
-/*   Updated: 2022/01/25 17:11:13 by hyeonsok         ###   ########.fr       */
+/*   Updated: 2022/01/25 17:23:25 by hyeonsok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,18 @@ int	main(void)
 	t_proc		proc1 = {"/bin/ls", (char *[]){"/bin/ls", "-l", NULL}, {0}, 0, 0, 0};
 	// t_proc		proc1 = {"/bin/cat", (char *[]){"/bin/cat", NULL}, {0}, 0, 0, 0};
 	t_proc		proc2 = {"/usr/bin/grep", (char *[]){"/usr/bin/grep", "a.out", NULL}, {0}, 0, 0, 0};
-	
-	//lexer
-	//parser
-	hx_array_push(&proc2.io_redirect, &io_file);
-	hx_array_push(&job.pipeline, &proc1);
-	hx_array_push(&job.pipeline, &proc2);
 
-	mush_exec(&job);
-	//debug
-	printf("last status: %d \n", job.last_status);
+	while (1)
+	{
+		//lexer
+		//parser
+		hx_array_push(&proc2.io_redirect, &io_file);
+		hx_array_push(&job.pipeline, &proc1);
+		hx_array_push(&job.pipeline, &proc2);
+
+		mush_exec(&job);
+		//debug
+		printf("last status: %d \n", job.last_status);
+	}
 	return (0);
 }
