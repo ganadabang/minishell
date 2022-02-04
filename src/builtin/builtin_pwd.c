3cc99cf122abf22eaa520b483d6f8a8903b4f972
gwpaeng <gwpaeng@naver.com>
2022-02-04 16:55:06 +0900

    Fix get pwd method

--- src/builtin/builtin_pwd.c
+++ src/builtin/builtin_pwd.c
@@ -6,7 +6,7 @@
 /*   By: gpaeng <gpaeng@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 16:32:39 by gpaeng            #+#    #+#             */
-/*   Updated: 2022/02/04 16:23:01 by gpaeng           ###   ########.fr       */
+/*   Updated: 2022/02/04 16:54:19 by gpaeng           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ int	builtin_pwd(t_state *state, int argc, char *argv[])
 	(void)argv;
 	if (getcwd(buf, 4096) == NULL)
 	{
-		pwd = ft_get_env("PWD");
+		// pwd = ft_get_env("PWD");
+		pwd = state->pwd;
 		printf("%s\n", pwd);
 		free(pwd);
 	}
