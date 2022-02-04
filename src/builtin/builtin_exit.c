9b03becf34190762c1d026313f4f8761bdd425ab
gwpaeng <gwpaeng@naver.com>
2022-02-04 17:00:18 +0900

    Feat: set not use parameter

--- src/builtin/builtin_exit.c
+++ src/builtin/builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: gpaeng <gpaeng@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 16:32:50 by gpaeng            #+#    #+#             */
-/*   Updated: 2022/02/04 16:22:48 by gpaeng           ###   ########.fr       */
+/*   Updated: 2022/02/04 16:58:25 by gpaeng           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ int	ft_str_alpha_check(char *str)
 
 int	builtin_exit(t_state *state, int argc, char *argv[])
 {
+	(void)state;
+	(void)argc;
 	printf("exit\n");
 	if (ft_cnt_arg(argv) > 1)
 	{
