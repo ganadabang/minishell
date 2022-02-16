02bafbe8932db874b392c2ac927ebb4393309a5c
hyeonsok <hyeonsok@student.42seoul.kr>
2022-02-17 05:42:59 +0900

    Fix echo

--- src/builtin/builtin_echo.c
+++ src/builtin/builtin_echo.c
@@ -3,71 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_echo.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gpaeng <gpaeng@student.42.fr>              +#+  +:+       +#+        */
+/*   By: hyeonsok <hyeonsok@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 16:34:22 by gpaeng            #+#    #+#             */
-/*   Updated: 2022/02/16 19:21:55 by gpaeng           ###   ########.fr       */
+/*   Updated: 2022/02/17 05:41:37 by hyeonsok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <unistd.h>
 #include "libft.h"
 #include "mush/parser.h"
 
-static int	ft_check_option(int str_length, char *str)
-{
-	int	idx;
-
-	idx = 1;
-	while (idx < str_length)
-	{
-		if (str[idx] != 'n')
-			return (0);
-		idx++;
-	}
-	return (1);
-}
-
-static int	ft_echo_option(char *str)
-{	
-	size_t	str_length;
-
-	str_length = ft_strlen(str);
-	if (ft_check_option(str_length, str))
-		return (1);
-	return (0);
-}
-
-void	ft_echo_print(char *argv[])
-{
-	size_t	i;
-
-	i = 0;
-	while (1)
-	{
-		if (argv[i] == NULL)
-			return ;
-		printf("%s", argv[i]);
-		if (argv != NULL)
-			printf(" ");
-		argv++;
-	}
-	return ;
-}
-
 int	builtin_echo(t_state *state, int argc, char *argv[])
 {
-	int	option;
+	t_buf	buffer;
+	int		option_n;
+	size_t	i;
 
-	option = 0;
-	argv++;
-	if (*argv && ft_echo_option(*argv))
+	ft_memset(&buffer, 0, sizeof(t_buf));
+	option_n = 0;
+	i = 0;
+	while (++i < argc)
 	{
-		argv++;
-		option = 1;
+		if (ft_memcmp(argv[i], "-n", 2) == 0 \
+			&& argv[i][1 + ft_strspn(&argv[i][1], "n")] == '\0')
+			option_n = 1;
+		else
+		{
+			hx_buffer_putstr(&buffer, argv[i], ft_strlen(argv[i]));
+			break ;
+		}
 	}
-	if (!ft_echo_option(*argv)) ft_echo_print(argv);
-	if (!option)
-		printf("\n");
+	while (++i < argc)
+	{
+		if (argv[i] != NULL)
+		{
+			hx_buffer_putchar(&buffer, ' ');
+			hx_buffer_putstr(&buffer, argv[i], ft_strlen(argv[i]));
+		}	
+	}
+	if (option_n == 0)
+		hx_buffer_putchar(&buffer, '\n');
+	write(1, buffer.data, buffer.len);
+	hx_buffer_cleanup(&buffer);
 	return (0);
 }
