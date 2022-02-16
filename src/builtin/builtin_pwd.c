0d0a7b8fe8d503f6cbff550b1c26ffea4fb59db6
hyeonsok <hyeonsok@student.42seoul.kr>
2022-02-17 06:21:37 +0900

    Fix pwd

--- src/builtin/builtin_pwd.c
+++ src/builtin/builtin_pwd.c
@@ -3,31 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_pwd.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyeonsok <hyeonsok@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hyeonsok <hyeonsok@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 16:32:39 by gpaeng            #+#    #+#             */
-/*   Updated: 2022/02/15 20:59:43 by hyeonsok         ###   ########.fr       */
+/*   Updated: 2022/02/17 06:11:54 by hyeonsok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include "mush.h"
 
 int	builtin_pwd(t_state *state, int argc, char *argv[])
 {
-	char	buf[4096];
-	char	*pwd;
-
-	(void)argv;
-	if (getcwd(buf, 4096) == NULL)
-	{
-		pwd = state->pwd;
-		printf("%s\n", pwd);
-		free(pwd);
-	}
-	else
-		printf("%s\n", buf);
+	printf("%s\n", state->pwd);
 	return (0);
 }
